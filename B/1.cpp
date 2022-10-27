b722fb803d71df7b08954efb02cc2869871115dc
nsalazard <61853949+nsalazard@users.noreply.github.com>
2022-10-27 14:46:55 -0500

    Update 1.cpp

--- B/1.cpp
+++ B/1.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 //---------- Constantes --------
 const int P = 8; //Numero de parámetros
-const int L = 1000; //Espacio 2L*2L
+const int L = 400; //Espacio 2L*2L
 const int K = 10;
-const double TMAX = 100;
+const double TMAX = 400;
 const int Ni = 1000;
+const int Nfood = 2;
 int Nlive = 2;
+int Energy = 0;
 //--- ------ Clases ------------
 class Bichin;
 class Selection;
@@ -31,6 +33,7 @@ public:
 	double GetE(void){return E;}; //inline
 	bool Alive(void){return E>0.0;};
   friend class Selection;
+	friend class Food;
 };
 
 void Bichin::Inicie(double x0,double y0,double E0,double m0, double R0){
@@ -55,6 +58,7 @@ void Bichin::Move(double K, Crandom ran64){
 		if(prob >= min && prob <= max){
 			x += K*std::cos(ii*(M_PI/4)); y += K*std::sin(ii*(M_PI/4));
 			E-=1;
+			Energy+=1;
 		}
 	}
 }
@@ -62,19 +66,54 @@ void Bichin::Move(double K, Crandom ran64){
 void Bichin::Dibujese(void){
   cout<<" , "<<x<<"+"<<R<<"*cos(t),"<<y<<"+"<<R<<"*sin(t)";
 }
-//--- clase Colisionador ----
+
+//---------FOOD-------------------------------------
+class Food{
+private:
+  double x,y, E, R, val;
+public:
+  void Inicie(double x0,double y0,double E0, double R0, double val0);
+	void Feed(Bichin & Bicho);
+	void Dibujese(void);
+	void Recharge(double newEnergy){E += newEnergy; Energy =0;};
+	friend class Selection;
+  friend class Bichin;
+};
+void Food::Inicie(double x0,double y0,double E0, double R0, double val0){
+	x = x0; y = y0;
+	R = R0;
+	E = E0;
+  val = val0;
+	}
+
+void Food::Feed(Bichin & Bicho){
+	double dis = sqrt(pow(Bicho.x-x,2.0)+pow(Bicho.y-y,2.0));
+	if (dis < (R+ Bicho.R)){
+		//cout << Bicho.E << "\n";
+		Bicho.E += val;
+		E -= val;
+		//cout << Bicho.E << "\n";
+	}
+}
+
+void Food::Dibujese(void){
+  cout<<" , "<<x<<"+"<<R<<"*cos(t),"<<y<<"+"<<R<<"*sin(t)";
+}
+
+
+//--- clase Selection----
 class Selection{
 private:
 public:
-  void Inicie(void); //Inicializa todos los individuos
-  void CalculeFuerzas(Bichin * Grano,int Nlive, double dt);
   void Birth(Bichin & BichoP,Bichin & BichoH, double t, int prob);
+	void RechargeFood(Food *food);
+friend class Food;
 };
 
 void Selection::Birth(Bichin & BichoP,Bichin & BichoH, double t,int prob){
 		Nlive +=1;
 		BichoP.E = BichoP.E/2;
-		BichoH.Inicie(0, 0, BichoP.E, 1, BichoP.R);
+		BichoH.Inicie(BichoP.x, BichoP.y, BichoP.E, 1, BichoP.R);
 		//Mutation
 	for(int ii=0; ii < P; ii++){
 		BichoH.moves[ii] = BichoP.moves[ii];
@@ -83,14 +122,16 @@ void Selection::Birth(Bichin & BichoP,Bichin & BichoH, double t,int prob){
 	//cout << BichoH.GetE() << "\n";
 }
 
+void  Selection::RechargeFood(Food *food){
+	double EnergyP= Energy/Nfood;
+	for(int ii=0; ii < Nfood; ii++){
+		food[ii].E += EnergyP;
+	}
+	Energy = 0;
+	};
 
-//---------FOOD-------------------------------------
-class Food{
-private:
-  double x,y, E;
-public:
-  void Inicie(void);
-};
+
+
 
 //----------------- Funciones de Animacion ----------
 void InicieAnimacion(void){
@@ -113,37 +154,58 @@ void TermineCuadro(void){
 //-----------  Programa Principal --------------  
 int main(void){
   Bichin Bichitos[Ni];
+	Food food[Nfood];
 	Selection Fate;
   Crandom ran64(1);
 	double R = 10.0;
-	int Ehijos = 100;
-	double Thijos = 20;
-	double prob1;
+	int Ehijos = 100; //Min Energy for reproduction
+	double Thijos = 20; //Min Time for reproduction
+	double Rfood = 10;
+	double prob1, prob2;
 	int prob;
 	
-	InicieAnimacion(); //Dibujar
-  
   //Inicializar los bichines
-  Bichitos[0].Inicie(100, 100, 300, 1,R);
-	Bichitos[1].Inicie(-100, -100, 100, 1,R);
+  Bichitos[0].Inicie(0, 0, 300, 1,R);
+	Bichitos[1].Inicie(0, 0, 100, 1,R);
+
+	//Inicializar la comida
+	food[0].Inicie(-50,0,10000,Rfood,5);
+	food[1].Inicie(50,0, 10000, Rfood,10);
+
+	InicieAnimacion(); //Dibujar
 
   for(double t=0, tdibujo=0; t<TMAX ; t+=1){  //Move  
     for(int ii=0; ii<Nlive; ii++){
-			if(Bichitos[ii].GetE() > 0){ //Bichitos[ii].Alive()
+			if(Bichitos[ii].Alive()){ //Bichitos[ii].Alive()
 				prob1 = 100*ran64.r();
 				Bichitos[ii].Move(K, prob1);
 				if(Bichitos[ii].GetE()>Ehijos && t > Thijos){
 				prob = int(P*ran64.r());
 				Fate.Birth(Bichitos[ii], Bichitos[Nlive], t, prob);
 					}
+				// Get some food
+				for(int jj=0; jj< Nfood; jj++){
+					food[jj].Feed(Bichitos[ii]);
+					}
 			}
 		}
      //if(tdibujo>tcuadro){
       InicieCuadro();
-			for(int ii=0;ii<Nlive;ii++) {Bichitos[ii].Dibujese();}
+			for(int ii=0;ii<Nlive;ii++){
+				if(Bichitos[ii].Alive()){
+					Bichitos[ii].Dibujese();
+					}
+				}
+			for(int ii=0;ii<Nfood;ii++){
+				food[ii].Dibujese();
+				}
       TermineCuadro();
      // tdibujo=0;
     //}
+		
+	//Recharge Food
+		Fate.RechargeFood(food);
+		
   }     
   return 0;
 }
