cfd3150f5ea6e0776aef8e7deaf195ccfd0aeb8b
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-02-20 11:21:41 -0500

    resultados_PCA

--- PCA_Data.py
+++ PCA_Data.py
@@ -4,26 +4,46 @@ import random as rd
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import matplotlib.pylab as plt
+TMAX=40000
+pca=PCA(n_components=8) #objeto PCA
+for i in range(int(TMAX/100)):
+	data2=pd.read_csv(f'Genes_datos/genes_{i*200}.csv', header = None)
+	data3=data2.T
+	data3
+	scaled_data = preprocessing.scale(data3.T)
 
+	
+	pca.fit(scaled_data)
+	pca_data=pca.transform(scaled_data)
 
-genes=['gene' + str(i) for i in range(0,100)]
+	per_var = np.round(pca.explained_variance_ratio_* 100, decimals=1)
+	labels = ['PC' + str(x) for x in range(1,len(per_var)+1)]
 
-wt = ['wt' + str(i) for i in range(0,5)]
-ko = ['ko' + str(i) for i in range(0,5)]
+	plt.bar(x=range(1,len(per_var)+1),height=per_var,tick_label=labels)
+	plt.ylabel('porcentaje de varianca')
+	plt.xlabel('componente principal')
+	plt.close()
+	#plt.show()
 
+	pca_df = pd.DataFrame(pca_data, columns=labels)
 
-data = pd.DataFrame(columns=[*wt,*ko] index=genes )
+	plt.scatter(pca_df.PC1,pca_df.PC2)
 
+	
+	plt.clf()
 
-for gene in data.index:
-	data.loc[gene,'wt0':'wt4'] = np.random.poisson( lam= rd.randrange(10,1000), size=5)
-	data.loc[gene,'ko0':'ko4'] = np.random.poisson( lam= rd.randrange(10,1000), size=5)
+	fig = plt.figure()
+	ax = fig.add_subplot(projection='3d')
+	ax.scatter(pca_df.PC1,pca_df.PC2,pca_df.PC3)
+	ax.set_xlim(-4,4)
+	ax.set_ylim(-4,4)
+	ax.set_zlim(-4,4)
 
-
-
-
-
-
-
-
+	ax.set_xlabel('PC1')
+	ax.set_ylabel('PC2')
+	ax.set_zlabel('PC3')
+	plt.title(f"PCA espacio genetico tiempo: {i*200}")
+	plt.savefig(f'per_var/im{i*200}')
+	plt.clf()
+	plt.close()
 
